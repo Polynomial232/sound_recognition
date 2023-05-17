f4e5dfaa5507bdeb5766ba6c2277ad8ec457d399
Polynomial232 <daffadhiya50@gmail.com>
2023-05-17 12:01:04 +0700

    install

--- install.sh
+++ install.sh
@@ -20,3 +20,4 @@ cp -R /home/app/sound_recognition_model/install/nltk_data /home/app/
 python3.8 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+rm -rf /home/app/sound_recognition_model
