80e79b26934f9347d9e7be46e94598868ab544d7
Polynomial232 <daffadhiya50@gmail.com>
2023-05-23 15:53:30 +0700

    main donwload

--- functions/update_model.py
+++ functions/update_model.py
@@ -6,12 +6,12 @@ def update_model():
     with open('text_classification/model/updated_at.txt', 'r', encoding='utf-8') as file:
         updated_at = file.read()
 
-    updated_at_github = requests.get('https://raw.githubusercontent.com/Polynomial232/sound_recognition/dev/text_classification/model/updated_at.txt')
+    updated_at_github = requests.get('https://raw.githubusercontent.com/Polynomial232/sound_recognition/main/text_classification/model/updated_at.txt')
 
     if updated_at != str(updated_at_github.json()):
-        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/dev/text_classification/model/intents.json', 'intents.json')
-        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/dev/text_classification/model/data.pth', 'data.pth')
-        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/dev/text_classification/model/updated_at.txt', 'updated_at.txt')
+        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/main/text_classification/model/intents.json', 'intents.json')
+        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/main/text_classification/model/data.pth', 'data.pth')
+        model_downloader('https://raw.githubusercontent.com/Polynomial232/sound_recognition/main/text_classification/model/updated_at.txt', 'updated_at.txt')
         print(f'{datetime.now()}\t model update done')
     else:
         print(f'{datetime.now()}\t model is already in the updated')
